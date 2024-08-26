e0a96e0d0151dee1287df91543bdd8df49212c23
parks-hordes <parks-hordes.0o@icloud.com>
2024-08-26 15:07:53 +0800

    move no level logic into grouplayer class

--- client/layer/Level.ts
+++ client/layer/Level.ts
@@ -11,17 +11,6 @@ export class Level extends GroupLayer {
     ) {
         super(name, x, y, tileLayersData);
     }
-    resetVisibility(): void {
-        const names = [...this.namesMap.values()].reduce<Set<string | undefined>>((prev, curr) => {
-            curr.forEach(o => prev.add(o));
-            return prev;
-        }, new Set())
-        this.node?.traverse(child => {
-            if (names.has(child.name)) {
-                child.visible = true
-            }
-        });
-    }
     getDirDownNames() {
         return [...this.namesMap.get("DirDown") || []]
     }
@@ -91,113 +80,6 @@ export class Level extends GroupLayer {
             }
             return find;
         })
-    }
-    initRenderTarget(tilesets: Tileset[], renderTarget: RenderTarget) {
-        const gridSize = tilesets[0].tilewidth;
-        const width = (this.max.x - this.min.x) * gridSize;
-        const height = (this.max.y - this.min.y) * gridSize;
-        renderTarget.setSize(width, height)
-    }
-    initGraphicsBuffer(
-        gl: OGLRenderingContext,
-        vertex: string,
-        fragment: string,
-        spriteVertex: string,
-        spriteFragment: string,
-        renderTarget: RenderTarget,
-        textures: Texture[],
-        internalIconName: string,
-        tilesets: Tileset[]
-    ) {
-        for (const tileLayer of this.tileLayers) {
-            if (tileLayer.name !== "Entities") {
-                tileLayer.drawLayer(tilesets, textures, renderTarget, gl, spriteVertex, spriteFragment);
-            }
-            tileLayer.initTileChunks(tilesets, gl, vertex, fragment, spriteVertex, spriteFragment, textures, internalIconName, this.namesMap)
-        }
-    }
-    initGraphics(renderTarget: RenderTarget, tilesets: Tileset[], gl: OGLRenderingContext, spriteVertex: string, spriteFragment: string, vertex: string, fragment: string) {
-        const gridSize = tilesets[0].tilewidth;
-        const offsetX = (this.x + (this.min.x * gridSize) + renderTarget.width / 2);
-        const offsetY = -(this.y + (this.min.y * gridSize) + renderTarget.height / 2);
-        {
-            const mesh = new Mesh(gl, {
-                geometry: new Plane(gl, {
-                    width: renderTarget.width,
-                    height: renderTarget.height,
-                }),
-                program: new Program(gl, {
-                    vertex: spriteVertex,
-                    fragment: spriteFragment,
-                    uniforms: {
-                        tMap: { value: renderTarget.texture }
-                    },
-                    frontFace: gl.CW,
-                    transparent: true
-                })
-            });
-            mesh.name = "test" + counterHandler.counter++;
-            mesh.position.x = offsetX;
-            mesh.position.y = offsetY;
-            mesh.rotation.x = Math.PI;
-            mesh.position.z = -radius;
-            mesh.updateMatrix();
-            mesh.setParent(this.node);
-            mesh.geometry.computeBoundingBox();
-            mesh.geometry.computeBoundingSphere();
-        }
-        {
-            const mesh = new Mesh(gl, {
-                geometry: new Plane(gl, {
-                    width: renderTarget.width,
-                    height: renderTarget.height,
-                }),
-                program: new Program(gl, {
-                    vertex,
-                    fragment,
-                    uniforms: {
-                        uColor: {
-                            value: new Vec3(0.4, 0.4, 0.4)
-                        }
-                    }
-                })
-            });
-            mesh.name = "test" + counterHandler.counter++;
-            mesh.setParent(this.node);
-            mesh.position.x = offsetX;
-            mesh.position.y = offsetY;
-            mesh.position.z = radius;
-            mesh.visible = false;
-            mesh.updateMatrix();
-            mesh.geometry.computeBoundingBox();
-            mesh.geometry.computeBoundingSphere();
-        }
-        {
-            const mesh = new Mesh(gl, {
-                geometry: new Plane(gl, {
-                    width: renderTarget.width,
-                    height: renderTarget.height,
-                }),
-                program: new Program(gl, {
-                    vertex,
-                    fragment,
-                    uniforms: {
-                        uColor: {
-                            value: new Vec3(0.4, 0.4, 0.4)
-                        }
-                    }
-                })
-            });
-            mesh.name = "test" + counterHandler.counter++;
-            mesh.setParent(this.node);
-            mesh.position.x = offsetX;
-            mesh.position.y = offsetY;
-            mesh.position.z = -radius;
-            mesh.visible = false;
-            mesh.updateMatrix();
-            mesh.geometry.computeBoundingBox();
-            mesh.geometry.computeBoundingSphere();
-        }
     }
 
 
